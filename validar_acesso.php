5c5e3223102fe7c788cc60b525af44cb0a3788e3
Lony <loonydarck@hotmail.com>
2022-11-01 14:02:23 -0300

    Otimizando a autenticação do usuário com a validação de acesso

--- validar_acesso.php
+++ validar_acesso.php
@@ -1,10 +1,34 @@
 <?php 
 
 
+require_once('db.class.php');
+
+
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
  
 $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
 
-echo $sql;
+
+$objDb = new db();
+$link = $objDb->conecta_mysql();
+
+$resultado_id = mysqli_query($link, $sql);
+
+if($resultado_id){
+    
+    $dados_usuario = mysqli_fetch_array($resultado_id);  //referência para uma informação externa para o PHP
+
+    if(isset($dados_usuario['usuario'])) { //testando o indice "usuario" do dado $dados_usuario que é um array contendo o retorno da consulta com o banco de dados.
+        echo 'Usuário existe';
+    } else {
+        header('Location: index.php?erro=1 '); //encaminhamento de parâmetros via URL
+    }
+
+} else {
+    echo 'Erro na execução da consulta, favor entrar em contato com o admin do site';
+}
+
+
+
 ?>
