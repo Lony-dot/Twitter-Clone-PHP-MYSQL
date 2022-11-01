4adbb769addaa292bf7fbd95a66a7dd5d464ef8e
Lony <loonydarck@hotmail.com>
2022-11-01 17:15:08 -0300

    Controlando sessão do usuário

--- validar_acesso.php
+++ validar_acesso.php
@@ -1,5 +1,6 @@
 <?php 
 
+session_start(); //esse comando indica para o script que ele terá acesso as variáveis de sessão
 
 require_once('db.class.php');
 
@@ -7,7 +8,7 @@ require_once('db.class.php');
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
  
-$sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
+$sql = "SELECT usuario, email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
 
 
 $objDb = new db();
@@ -20,7 +21,12 @@ if($resultado_id){
     $dados_usuario = mysqli_fetch_array($resultado_id);  //referência para uma informação externa para o PHP
 
     if(isset($dados_usuario['usuario'])) { //testando o indice "usuario" do dado $dados_usuario que é um array contendo o retorno da consulta com o banco de dados.
-        echo 'Usuário existe';
+
+        $_SESSION['usuario'] = $dados_usuario['usuario'];
+        $_SESSION['email'] = $dados_usuario['email'];
+
+       header('Location: home.php');
+
     } else {
         header('Location: index.php?erro=1 '); //encaminhamento de parâmetros via URL
     }
