eed5c95f2681e75f7659536cf0702d6d10a8984b
Lony <loonydarck@hotmail.com>
2022-11-03 14:28:56 -0300

    Bloqueio de inclusão de usuários já cadastrados

--- registra_usuario.php
+++ registra_usuario.php
@@ -9,11 +9,48 @@
     $objDb = new db();
     $link = $objDb->conecta_mysql();
 
+    //Verificar se o usuário já existe.
+    //essa etapa precisa ser realizada somente após o $objDb e a coneção do banco, conecta_mysql()
+    $sql = "select * from usuarios where usuario = '$usuario' ";
+    if( $resultado_id = mysqli_query($link, $sql)) {
 
-    $con = " insert into usuarios(usuario, email, senha) values ('$usuario', '$email', '$senha') ";
+      $dados_usuario =  mysqli_fetch_array($resultado_id);
+
+      if(isset($dados_usuario['usuario'])){
+        echo 'Usuário já cadastrado';
+      } else {
+        echo 'Usuário não cadastrado, ok, pode cadastrar <br /><br />';
+      }
+
+    } else {
+        echo 'Erro ao tentar localizar o registro do usuário <br /><br />';
+    }
+
+
+    //Verificar se o e-mail já existe.
+    $sql = "select * from usuarios where email = '$email' ";
+    if( $resultado_id = mysqli_query($link, $sql)) {
+
+      $dados_usuario =  mysqli_fetch_array($resultado_id);
+
+      if(isset($dados_usuario['email'])){
+        echo 'E-mail já cadastrado';
+      } else {
+        echo 'E-mail não cadastrado, ok, pode cadastrar <br /><br />';
+      }
+
+    } else {
+        echo 'Erro ao tentar localizar o registro do E-mail <br /><br />';
+    }
+
+
+    
+
+    die();
+    $sql = " insert into usuarios(usuario, email, senha) values ('$usuario', '$email', '$senha') ";
 
     //executar a query
-   if ($result = mysqli_query($link, $con)){
+   if ($result = mysqli_query($link, $sql)){
     echo 'Usuário registrado com sucesso!';
    } else {
     echo 'Erro ao registrar o usuário!';
